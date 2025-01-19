3244a70848afb7915de0d860afe1ca2fae65e41b
Gaurav Upadhyay <gauravupadhyay20112003@gmail.com>
2025-01-19 17:14:00 +0530

    nav-ui

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const hamburger = document.getElementById("hamburger");
 const navLinks = document.getElementById("nav_links");
 
 hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
   navLinks.classList.toggle("active");
 });
 
